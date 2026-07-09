4521773b589e4b0de60079c08f34565d357b4487
younesnemouchi2-tech <younesnemouchi2@gmail.com>
2026-07-10 00:19:52 +0100

    feat: add PwaInstallPrompt to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ServiceWorkerRegister } from '@/components/sw-register'
+import { PwaInstallPrompt } from '@/components/pwa-install-prompt'
 import { Toaster } from '@/components/ui/toaster'
 import HomeContent from '@/components/home-content'
 
@@ -13,6 +14,7 @@ export default function Home() {
         <HomeContent />
         <Toaster />
         <ServiceWorkerRegister />
+        <PwaInstallPrompt />
       </ThemeProvider>
     </SessionProvider>
   )
